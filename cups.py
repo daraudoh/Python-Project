419733ebfc8dc3955d461dcf295ceea23d76fd67
daraudoh <162847659+daraudoh@users.noreply.github.com>
2024-04-19 20:16:44 -0700

    Creating first parts of program

---
+++ cups.py
@@ -0,0 +1,4 @@
+price = input("How much does a cup of coffee cost? ")
+cups = input("How many cups of coffee would you like? ")
+total = float(price) * int(cups)
+print("Your total is $" + total + " for " + cups + " cups of coffee.")
