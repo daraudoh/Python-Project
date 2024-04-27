5c1978fb4317f89691e879e5434220f65a846256
daraudoh <162847659+daraudoh@users.noreply.github.com>
2024-04-27 02:53:56 -0700

    Making changes to the file

---
+++ coffee.py
@@ -0,0 +1,5 @@
+print("Let's collet some information before we start the game.\n")
+
+#Get name and shop name
+name = input("What is your name? ")
+shop_name = 
---
+++ scratch.py
@@ -0,0 +1,6 @@
+print("Hello, \nWorld") #\n creates a new line in the middle of the string
+
+print("Hello, World!\n\nGoodbye, World!")
+
+name = "Dara"
+print(name[:1])
